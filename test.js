ed9a35a3c4ee8ab20dc0a647b26b9e3cdf99dc35
Mr.Robot <50441494+admingraph1@users.noreply.github.com>
2024-08-28 16:30:39 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const twApi = require('./index2')
 setTimeout(async () => {
-    a = await twApi('https://gift.truemoney.com/campaign/?v=???', '0640288999')
+    a = await twApi('https://gift.truemoney.com/campaign/?v=???', 'phone')
     console.log(a.status.code)
 })
